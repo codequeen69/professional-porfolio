2e73d0a0f0457043c8afeeba5553712f2499c13c
Venus DM <crhowe90@gmail.com>
2022-10-08 13:43:55 -0500

    About section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import camilleImage from '../../assets/profile.jpg'
 
 function About (){
     return(
-        <div>
-
-        </div>
+        <section>
+            
+            <h1>Allow Me to Introduce Myself...</h1>
+            <img src={camilleImage} className='my-2' />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero minima iste optio qui commodi dolorum
+            ducimus aliquid placeat eaque maiores, itaque ullam laboriosam corrupti. Sed pariatur unde deleniti
+            veritatis quibusdam?
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolor quos illo quasi ipsam, rerum modi recusandae
+            nesciunt impedit earum iusto explicabo. Doloribus officia similique recusandae laboriosam, quis enim omnis
+            aperiam!</p>
+            
+        </section>
     );
 
 }
